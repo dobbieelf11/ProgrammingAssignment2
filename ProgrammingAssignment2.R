3e2a349a97ba8e0265235c00ba1c05bdcbee66aa
P.A <70672163+dobbieelf11@users.noreply.github.com>
2020-12-02 02:51:39 +0530

    Update ProgrammingAssignment2.R

--- ProgrammingAssignment2.R
+++ ProgrammingAssignment2.R
@@ -1,17 +1,19 @@
+#Creating a matrix object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
-        m <- NULL
+        m <- NULL                                       #initializing m as NULL to hold value of inverse of matrix
         set <- function(y) {
                 x <<- y
-                m <<- NULL
+                m <<- NULL                              #Incase of new matrix, assign m to NULL
         }
-        get <- function() x
-        setinverse <- function(inverse) m <<- inverse
-        getinverse <- function() m
+        get <- function() x                             #Returns value of the matrix
+        setinverse <- function(inverse) m <<- inverse   #assigns value
+        getinverse <- function() m                      #gets the value of m
         list(set = set, get = get,
              setinverse = setinverse,
              getinverse = getinverse)
 }
-
+#This function computes the inverse of the matrix created above,
+#if inverse is already calculated the it retrieves inverse from cache
 cacheSolve <- function(x, ...) {
         m <- x$getinverse()
         if(!is.null(m)) {
